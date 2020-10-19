d8698b0e0545d60e0a2c9a7cc2df152474701edc
Rodion <rodion.gramushniak@gmail.com>
2020-10-19 23:31:43 +0300

    Work on lvl 1
    Added styles to the back button ....

---
+++ app/src/main/java/space/quiz/Level1.java
@@ -0,0 +1,13 @@
+package space.quiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Level1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.universal);
+    }
+}

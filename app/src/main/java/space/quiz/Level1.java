b8f2967fa80d683043f957c9cf0ebd9d50fac403
Rodion <rodion.gramushniak@gmail.com>
2020-10-20 11:49:08 +0300

    Level marking is complete
    Not all items move to the center of the screen

--- app/src/main/java/space/quiz/Level1.java
+++ app/src/main/java/space/quiz/Level1.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class Level1 extends AppCompatActivity {
 
@@ -11,6 +12,12 @@ public class Level1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universal);
+
+        //код который скругляет углы картинок
+        final ImageView img_left = findViewById(R.id.img_left);
+        img_left.setClipToOutline(true);
+        final ImageView img_right = findViewById(R.id.img_right);
+        img_right.setClipToOutline(true);
 
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);

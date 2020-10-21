7ae33f0c28f904226c2bb06d60cdd49a079da8bb
Rodion <rodion.gramushniak@gmail.com>
2020-10-21 13:19:26 +0300

    Dialog is done

--- app/src/main/java/space/quiz/Level1.java
+++ app/src/main/java/space/quiz/Level1.java
@@ -1,12 +1,22 @@
 package space.quiz;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Level1 extends AppCompatActivity {
+
+    Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +29,50 @@ public class Level1 extends AppCompatActivity {
         final ImageView img_right = findViewById(R.id.img_right);
         img_right.setClipToOutline(true);
 
+        //убрать верхнюю полосу
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //вызов диалогового окна в начале игры
+        dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);   //убрать заголовок
+        dialog.setContentView(R.layout.preview_dialog);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT)); //прозрачный фон диалогового окна
+        dialog.setCancelable(false);    //окно нельзя закрыть кнопкой "назад"
+        dialog.show();
+
     }
+
+     public void closeDialog(View view){
+        try{
+            Intent intent = new Intent(Level1.this, GameLevels.class);
+            startActivity(intent);
+            finish();
+        }catch (Exception e){
+
+        }
+        dialog.dismiss();   //закрыть диалоговое окно
+    }
+
+    public void continueGame(View view){
+        dialog.dismiss(); //закрыть диалоговое окно
+    }
+
 }
+
+
+
+
+/*btnClose.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View view) {
+        try{
+        //Intent intent = new Intent(Level1.this, GameLevels.class);
+        //startActivity(intent);
+        //finish();
+        }catch (Exception e){
+
+        }
+        //dialog.dismiss();   //закрыть диалоговое окно
+        }
+        });*/

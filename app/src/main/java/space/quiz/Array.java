a778d6f88fade98dc75e9a8eb0341a281dac754a
Rodion <rodion.gramushniak@gmail.com>
2020-10-22 22:50:55 +0300

    Buttons on first level work correct
    add array.java , listeners for left and right buttons

--- app/src/main/java/space/quiz/Array.java
+++ app/src/main/java/space/quiz/Array.java
@@ -1,9 +1,36 @@
 package space.quiz;
 
-import android.graphics.drawable.BitmapDrawable;
 
 public class Array {
 
     final int[] images1 = {
+        R.drawable.first_lvl_zero,
+        R.drawable.first_lvl_one,
+        R.drawable.first_lvl_two,
+        R.drawable.first_lvl_three,
+        R.drawable.first_lvl_four,
+        R.drawable.first_lvl_five,
+        R.drawable.first_lvl_six,
+        R.drawable.first_lvl_seven,
+        R.drawable.first_lvl_eight,
+        R.drawable.first_lvl_nine,
     };
+
+    final int[] texts1 = {
+            R.string.lvl1text0,
+            R.string.lvl1text1,
+            R.string.lvl1text2,
+            R.string.lvl1text3,
+            R.string.lvl1text4,
+            R.string.lvl1text5,
+            R.string.lvl1text6,
+            R.string.lvl1text7,
+            R.string.lvl1text8,
+            R.string.lvl1text9,
+    };
+
+    /*public Drawable getDrawableFromAssets(String path) throws IOException {
+        Drawable img = Drawable.createFromStream(getAssets().open(path), null);
+        return img;
+    }*/
 }
--- app/src/main/java/space/quiz/Level1.java
+++ app/src/main/java/space/quiz/Level1.java
@@ -1,27 +1,47 @@
 package space.quiz;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.util.Random;
+
 public class Level1 extends AppCompatActivity {
 
     Dialog dialog;
 
+    public int numLeft;
+    public int numRight;
+    Array array = new Array();
+    Random random = new Random();
+
+    public int count = 0;   //счетчик правильных ответов
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universal);
+
+        //убрать верхнюю полосу
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         //установка текста уровня
         TextView textViewOfLevel = findViewById(R.id.text_levels);
@@ -33,9 +53,9 @@ public class Level1 extends AppCompatActivity {
         final ImageView img_right = findViewById(R.id.img_right);
         img_right.setClipToOutline(true);
 
-        //убрать верхнюю полосу
-        Window w = getWindow();
-        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //путь к левой и правой TextView
+        final TextView text_left = findViewById(R.id.text_left);
+        final TextView text_right = findViewById(R.id.text_right);
 
         //вызов диалогового окна в начале игры
         dialog = new Dialog(this);
@@ -61,6 +81,173 @@ public class Level1 extends AppCompatActivity {
             }
         });
 
+        //масив c текствювов на полосе, для прогресса игры
+        final int[] progress = {
+                R.id.point1, R.id.point2, R.id.point3, R.id.point4, R.id.point5,
+                R.id.point6, R.id.point7, R.id.point8, R.id.point9, R.id.point10
+        };
+
+
+        //заполнения рандомно картинки и подписей
+        numLeft = random.nextInt(10);
+        img_left.setImageResource(array.images1[numLeft]);
+        text_left.setText(array.texts1[numLeft]);
+
+        numRight = random.nextInt(10);
+        while (numRight == numLeft){    //чтобы числа были разные
+            numRight = random.nextInt(10);
+        }
+        img_right.setImageResource(array.images1[numRight]);
+        text_right.setText(array.texts1[numRight]);
+
+        //подключаем анимацию
+        final Animation a = AnimationUtils.loadAnimation(Level1.this, R.anim.alpha);
+
+        //нажатия на левую картинку
+        img_left.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event){
+                if (event.getAction() == MotionEvent.ACTION_DOWN){
+                    img_right.setEnabled(false);    //блокируем правую картинку
+                    if (numLeft > numRight){
+                        img_left.setImageResource(R.drawable.img_true);
+                    }else {
+                        img_left.setImageResource(R.drawable.img_false);
+                    }
+                }
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (numLeft > numRight) {
+                        if (count < 10) count++;    //защитуем правильный ответ
+
+                        // закрашиваем прогресс серым цветом
+                        for (int i = 0; i < 10; i++) {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points);
+                        }
+                        //опредиляем правильные ответы и закрашивем зеленым
+                        for (int i = 0; i < count; i++) {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points_green);
+                        }
+
+                    } else//если левая картинка меньше
+                    {
+                        if (count > 0) {
+                            if (count == 1) {
+                                count = 0;
+                            } else {
+                                count = count - 2;
+                            }
+                        }
+                        // закрашиваем прогресс серым цветом
+                        for (int i = 0; i < 9; i++) {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points);
+                        }
+                        //опредиляем правильные ответы и закрашивем зеленым
+                        for (int i = 0; i < count; i++) {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points_green);
+                        }
+                    }
+
+                    if (count == 10) {
+                        //выход из уровня
+                    }else   {
+                        //заполнения рандомно картинки и подписей
+                        numLeft = random.nextInt(10);
+                        img_left.setImageResource(array.images1[numLeft]);
+                        img_left.startAnimation(a);
+                        text_left.setText(array.texts1[numLeft]);
+
+                        numRight = random.nextInt(10);
+                        while (numRight == numLeft) {    //чтобы числа были разные
+                            numRight = random.nextInt(10);
+                        }
+                        img_right.setImageResource(array.images1[numRight]);
+                        img_right.startAnimation(a);
+                        text_right.setText(array.texts1[numRight]);
+
+                        img_right.setEnabled(true); // включаем обратно правую картинку
+                    }
+
+                }
+                return true;
+            }
+        });
+
+        //нажатия на правую картинку
+        img_right.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event){
+                if (event.getAction() == MotionEvent.ACTION_DOWN){
+                    img_left.setEnabled(false);    //блокируем левую картинку
+                    if (numRight > numLeft){
+                        img_right.setImageResource(R.drawable.img_true);
+                    }else {
+                        img_right.setImageResource(R.drawable.img_false);
+                    }
+                }
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (numRight > numLeft) {
+                        if (count < 10) count++;    //защитуем правильный ответ
+
+                        // закрашиваем прогресс серым цветом
+                        for (int i = 0; i < 10; i++) {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points);
+                        }
+                        //опредиляем правильные ответы и закрашивем зеленым
+                        for (int i = 0; i < count; i++) {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points_green);
+                        }
+
+                    } else//если левая картинка меньше
+                    {
+                        if (count > 0) {
+                            if (count == 1) {
+                                count = 0;
+                            } else {
+                                count = count - 2;
+                            }
+                        }
+                        // закрашиваем прогресс серым цветом
+                        for (int i = 0; i < 9; i++) {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points);
+                        }
+                        //опредиляем правильные ответы и закрашивем зеленым
+                        for (int i = 0; i < count; i++) {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points_green);
+                        }
+                    }
+
+                    if (count == 10) {
+                        //выход из уровня
+                    }else   {
+                        //заполнения рандомно картинки и подписей
+                        numLeft = random.nextInt(10);
+                        img_left.setImageResource(array.images1[numLeft]);
+                        img_left.startAnimation(a);
+                        text_left.setText(array.texts1[numLeft]);
+
+                        numRight = random.nextInt(10);
+                        while (numRight == numLeft) {    //чтобы числа были разные
+                            numRight = random.nextInt(10);
+                        }
+                        img_right.setImageResource(array.images1[numRight]);
+                        img_right.startAnimation(a);
+                        text_right.setText(array.texts1[numRight]);
+
+                        img_left.setEnabled(true); // включаем обратно правую картинку
+                    }
+
+                }
+                return true;
+            }
+        });
     }
 
     public void closeDialog(View view){
@@ -88,21 +275,12 @@ public class Level1 extends AppCompatActivity {
 
         }
     }
+
+
+
+    //перевірка
+    public Drawable getDrawableFromAssets(String path) throws IOException {
+        Drawable img = Drawable.createFromStream(getAssets().open(path), null);
+        return img;
+    }
 }
-
-
-
-
-/*btnClose.setOnClickListener(new View.OnClickListener() {
-@Override
-public void onClick(View view) {
-        try{
-        //Intent intent = new Intent(Level1.this, GameLevels.class);
-        //startActivity(intent);
-        //finish();
-        }catch (Exception e){
-
-        }
-        //dialog.dismiss();   //закрыть диалоговое окно
-        }
-        });*/
